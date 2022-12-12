3e9e03475f0907a59609ccb06d2c2e11ea0ca53b
MasaiSchoo <30krishnakumar111998@gmail.com>
2022-12-12 20:14:35 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+      if (this.scrollY > 20) {
+        $(".navbar").addClass("sticky");
+      } else {
+          $(".navbar").removeClass("sticky");
+      }
+      if(this.scroll>500){
+        $(".scroll-up-btn").addClass("show");
+      }else{
+        $(".scroll-up-btn").removeClass("show");
+      }
+    });
+  
+  
+  // slide-up Script
+  $(".scroll-up-btn").click(function(){
+    $('html').animate({scrollTop: 0})
+  });
+  
+  //   toggle
+  
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  })
+  
+  // typing Animation
+  
+  var typed = new Typed(".typing",{
+    strings:["Full Stack Web Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+  })
+  var typed = new Typed(".typing-2",{
+    strings:["Full Stack Web Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+  })
+  
+  // owl carousel
+  
+  $('.carousel').owlCarousel({
+    margin:20,
+    loop:true,
+    autoplayTimeOut:2000,
+    autoplayHoverPause:true,
+    responsive:{
+      0:{
+        items:1,
+        nav:false
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1000:{
+        items:3,
+        nav:false
+      }
+    }
+  })
+  });
+  
